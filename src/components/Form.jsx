c07dddbba6be3e46bb029b5810ba4049904efb4c
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-07 18:06:49 +0500

    To create is app for my own practise and learning and created a form validation to handle the input of form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
+    const [inputData, setInputData] = useState({
+        firstName : "",
+        lastName : "",
+        fName : "",
+        cnic : "",
+        email : "",
+        password : "",
+        hobbies : "",
+        gender : "",
+    })
+
+    const handleFormData = (e) =>{
+        const {name, value} = e.target
+        setInputData((preData) =>({
+            ...preData, [name]: value,
+        }));
+    };
+    
+    const formSubmit = (e) =>{
+        e.preventDefault()
+        console.log("Form data submitted" , inputData)
+    }
+
   return (
     <div className="bg-white rounded-lg w-full max-w-xl mx-auto p-5 mt-5 shadow-xl">
       <h1 className="text-center font-bold text-2xl mb-5">Registration Form</h1>
-      <form>
+      <form >
         <div className="flex flex-col sm:flex-row gap-4 mb-3">
           <div className="flex flex-col w-full sm:w-1/2">
             <label className="text-md font-semibold" htmlFor="first-name">
               First Name
             </label>
             <input
-              id="first-name"
+              id="firstName"
+              name="firstName"
+              value={inputData.firstName}
+              onChange={handleFormData}
               type="text"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Enter your name"
@@ -23,7 +49,10 @@ const Form = () => {
               Last Name
             </label>
             <input
-              id="last-name"
+              id="lastName"
+              name="lastName"
+              value={inputData.lastName}
+              onChange={handleFormData}
               type="text"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Enter your last name"
@@ -37,7 +66,10 @@ const Form = () => {
               Father's Name
             </label>
             <input
-              id="father-name"
+              id="fName"
+              name="fName"
+              value={inputData.fName}
+              onChange={handleFormData}
               type="text"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Father's name"
@@ -49,6 +81,9 @@ const Form = () => {
             </label>
             <input
               id="cnic"
+              name="cnic"
+              value={inputData.cnic}
+              onChange={handleFormData}
               type="number"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="CNIC number"
@@ -63,6 +98,9 @@ const Form = () => {
             </label>
             <input
               id="email"
+              name="email"
+              value={inputData.email}
+              onChange={handleFormData}
               type="email"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Enter your email"
@@ -74,6 +112,9 @@ const Form = () => {
             </label>
             <input
               id="password"
+              name="password"
+              value={inputData.password}
+              onChange={handleFormData}
               type="password"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Enter your password"
@@ -88,8 +129,10 @@ const Form = () => {
             </label>
             <input
               id="hobbies"
+              name="hobbies"
+              value ={inputData.hobbies}
+              onChange={handleFormData}
               type="text"
-              value="cricket, volleyball, hockey, table tennis"
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 transition duration-150"
               placeholder="Enter your hobbies"
               readOnly
@@ -101,6 +144,9 @@ const Form = () => {
             </label>
             <select
               id="gender"
+              name="gender"
+              value={inputData.gender}
+              onChange={handleFormData}
               className="w-full p-2 outline-none border border-gray-300 rounded-lg focus:border-indigo-500 bg-white cursor-pointer transition duration-150"
             >
               <option value="">-- Select --</option>
@@ -114,7 +160,9 @@ const Form = () => {
         <div>
           <button
             type="submit"
+            
             className="w-full p-2 outline-none border border-indigo-500 rounded-lg bg-indigo-600 text-white font-semibold hover:bg-indigo-700 transition duration-300 transform hover:scale-105"
+          onClick={formSubmit}
           >
             Submit
           </button>
