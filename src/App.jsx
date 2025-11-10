ddfaa1aeba840642e81c7c3d77335a01519d03a9
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-10 18:01:32 +0500

    Make a few changes form component and five component changes as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import Component from './components/Component'
-import Jsx from './components/Jsx'
-import Form from './components/Form'
+import { useState } from "react";
+import Component from "./components/Component";
+import Jsx from "./components/Jsx";
+import Form from "./components/Form";
 
 function App() {
- return(
-  <div className='bg-gray-200 p-5 mx-auto min-h-screen '>
-    <Form/>
-    <Component/>
-    <Jsx />
-  </div>
- )
+  return (
+    <div className="bg-gray-200 p-5 mx-auto min-h-screen ">
+      <Form />
+      <Component />
+      <Jsx />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Jsx.jsx
+++ src/components/Jsx.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Jsx = () => {
-  return (
-    <div>
-      {/* Jsx */}
-    </div>
-  )
-}
+  return <div>{/* Jsx */}</div>;
+};
 
-export default Jsx
+export default Jsx;
