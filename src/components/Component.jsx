3873a7da58510e9cb4ab6b9702bfbed1bbb67eaa
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-11 23:44:00 +0500

    To create and a few change

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Component = () => {
-    const [count, setCount] = useState()
-    
-    return (
-        <div>
-            <h1>Count:{count}</h1>
-            <button onClick={()=> setCount(count + 1)}>Add the count</button>
-        </div>
-    )
-}
+  const [count, setCount] = useState();
+
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={() => setCount(count + 1)}>Add the count</button>
+    </div>
+  );
+};
 
 export default Component;
--- src/components/Jsx.jsx
+++ src/components/Jsx.jsx
@@ -2,19 +2,17 @@ import React from "react";
 
 const Jsx = () => {
   const fiurt = "welcome";
-  const vegetable = ['onion', 'tomato', 'potato', 'garlic']
-  return(
+  const vegetable = ["onion", "tomato", "potato", "garlic"];
+  return (
     <div>
       <h1>{fiurt}</h1>
-      <ul style={{fontSize:''}}>
-      {
-        vegetable.map((item,index) =>(
+      <ul style={{ fontSize: "" }}>
+        {vegetable.map((item, index) => (
           <li key={index}>{item}</li>
-        ))
-      }
+        ))}
       </ul>
     </div>
-  )
+  );
 };
 
 export default Jsx;
