ffb25d2d99b9ec5cb5a76d86d0aae83a85c72236
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-11 23:29:09 +0500

    To create and a few change

--- src/components/Jsx.jsx
+++ src/components/Jsx.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Jsx = () => {
-  return <div>{/* Jsx */}</div>;
+  const fiurt = "welcome";
+  const vegetable = ['onion', 'tomato', 'potato', 'garlic']
+  return(
+    <div>
+      <h1>{fiurt}</h1>
+      <ul style={{fontSize:''}}>
+      {
+        vegetable.map((item,index) =>(
+          <li key={index}>{item}</li>
+        ))
+      }
+      </ul>
+    </div>
+  )
 };
 
 export default Jsx;
