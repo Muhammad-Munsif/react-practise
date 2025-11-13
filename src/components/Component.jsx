bb424d07611ef2a1ed423059c206b440e030388b
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-13 23:13:00 +0500

    small changes table

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -7,9 +7,20 @@ const Component = () => {
     <div>
       <h1>Count:{count}</h1>
       <div className="flex gap-3">
-      <button onClick={() => setCount(count + 5)} className="bg-amber-300 text-2xl px-4 py-2 text-indigo-700 rounded-lg outline-none hover:cursor-pointer ">Add the count</button>
-      <button onClick={() => setCount(count - 5)} className="bg-amber-300 text-2xl px-4 py-2 text-indigo-700 rounded-lg outline-none hover:cursor-pointer ">minus the count</button>
+        <button
+          onClick={() => setCount(count + 5)}
+          className="bg-amber-300 text-2xl px-4 py-2 text-indigo-700 rounded-lg outline-none hover:cursor-pointer "
+        >
+          Add the count
+        </button>
+        <button
+          onClick={() => setCount(count - 5)}
+          className="bg-amber-300 text-2xl px-4 py-2 text-indigo-700 rounded-lg outline-none hover:cursor-pointer "
+        >
+          minus the count
+        </button>
       </div>
+      
     </div>
   );
 };
