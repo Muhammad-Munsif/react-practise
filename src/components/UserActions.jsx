bc2cfbc05ec22e96d2b58bad7479cf30099ce9f9
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-24 16:52:22 +0500

    changes in Useractions component

--- src/components/UserActions.jsx
+++ src/components/UserActions.jsx
@@ -2,23 +2,21 @@ import React from "react";
 
 const UserActions = ({ onEdit, onDelete }) => {
   return (
-    
     <div className="flex gap-2">
-  <button
-    onClick={onEdit}
-    className="bg-yellow-500 hover:bg-yellow-600 transition text-white px-4 py-1 rounded-lg shadow-sm"
-  >
-    Edit
-  </button>
+      <button
+        onClick={onEdit}
+        className="bg-yellow-500 hover:bg-yellow-600 transition text-white px-4 py-1 rounded-lg shadow-sm"
+      >
+        Edit
+      </button>
 
-  <button
-    onClick={onDelete}
-    className="bg-red-600 hover:bg-red-700 transition text-white px-4 py-1 rounded-lg shadow-sm"
-  >
-    Delete
-  </button>
-</div>
-
+      <button
+        onClick={onDelete}
+        className="bg-red-600 hover:bg-red-700 transition text-white px-4 py-1 rounded-lg shadow-sm"
+      >
+        Delete
+      </button>
+    </div>
   );
 };
 
