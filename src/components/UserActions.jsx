f295169faf66078af88439be53861551d2060dc6
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-11-24 17:49:16 +0500

    changes in UsersForm component

--- src/components/UserActions.jsx
+++ src/components/UserActions.jsx
@@ -2,22 +2,21 @@ import React from "react";
 
 const UserActions = ({ onEdit, onDelete }) => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 mt-4">
       <button
         onClick={onEdit}
-        className="bg-yellow-500 hover:bg-yellow-600 transition text-white px-4 py-1 rounded-lg shadow-sm"
+        className="px-4 py-1 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 transition"
       >
         Edit
       </button>
 
       <button
         onClick={onDelete}
-        className="bg-red-600 hover:bg-red-700 transition text-white px-4 py-1 rounded-lg shadow-sm"
+        className="px-4 py-1 bg-red-600 text-white rounded-lg hover:bg-red-700 transition"
       >
         Delete
       </button>
     </div>
   );
 };
-
 export default UserActions;
