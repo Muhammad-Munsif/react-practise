249272f0ecebbc613de33a66507a90e3114fdf1b
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-12-04 23:57:16 +0500

    assest this is complete changes in layout component

--- src/components/ButtonDemo.jsx
+++ src/components/ButtonDemo.jsx
@@ -8,7 +8,13 @@ const ButtonDemo = () => {
       <Button>Default Button</Button>
       <Button type="dashed">Dashed Button</Button>
       <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
       <Button type="link">Link Button</Button>
+      <Button type="primary" icon={<DownloadOutlined />}>
+      <Button type="primary" icon={<DownloadOutlined />}>
+      <Button type="primary" icon={<DownloadOutlined />}>
       <Button type="primary" icon={<DownloadOutlined />}>
         Download
       </Button>
