fa7b5a95ff8883c30c4d11caaa9a8c372333fbdd
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-12-04 23:57:37 +0500

    assest this is complete changes in layout component

--- src/components/ButtonDemo.jsx
+++ src/components/ButtonDemo.jsx
@@ -27,3 +27,32 @@ const ButtonDemo = () => {
 };
 
 export default ButtonDemo;
+import { Button, Space } from "antd";
+import { DownloadOutlined, SearchOutlined } from "@ant-design/icons";
+
+const ButtonDemo = () => {
+  return (
+    <Space wrap>
+      <Button type="primary">Primary Button</Button>
+      <Button>Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+      <Button type="primary" icon={<DownloadOutlined />}>
+      <Button type="primary" icon={<DownloadOutlined />}>
+      <Button type="primary" icon={<DownloadOutlined />}>
+      <Button type="primary" icon={<DownloadOutlined />}>
+        Download
+      </Button>
+      <Button icon={<SearchOutlined />}>Search</Button>
+      <Button type="primary" danger>
+        Danger
+      </Button>
+    </Space>
+  );
+};
+
+export default ButtonDemo;
